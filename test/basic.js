d6645124aeb30455dfccc9a048423977624d5d69
Shaquu <tadev3@gmail.com>
2019-10-08 19:07:11 +0200

    Prettify source code

--- test/basic.js
+++ test/basic.js
@@ -1,18 +1,18 @@
-("use strict");
+('use strict')
 
-const mocha = require("mocha");
-const describe = mocha.describe;
-const it = mocha.it;
+const mocha = require('mocha')
+const describe = mocha.describe
+const it = mocha.it
 
-const assert = require("assert");
+const assert = require('assert')
 
-let RED = require("./node-red-mock.js").RED;
+let RED = require('./node-red-mock.js').RED
 
-describe("homekit", function() {
-    describe("#loading", function() {
-        it("should load without error", function() {
-            require("../homekit.js")(RED);
-            assert.ok(true);
-        });
-    });
-});
+describe('homekit', function() {
+    describe('#loading', function() {
+        it('should load without error', function() {
+            require('../homekit.js')(RED)
+            assert.ok(true)
+        })
+    })
+})
--- test/node-red-mock.js
+++ test/node-red-mock.js
@@ -1,58 +1,57 @@
-const CONFIG = {};
-CONFIG.bridgeName = "bridgeName";
-CONFIG.pinCode = "pinCode";
-CONFIG.port = "port";
-CONFIG.allowInsecureRequest = "allowInsecureRequest";
-CONFIG.manufacturer = "manufacturer";
-CONFIG.serialNo = "serialNo";
-CONFIG.model = "model";
-CONFIG.name = "name";
-CONFIG.serviceName = "Lightbulb";
-CONFIG.parentService = "ParentService";
+const CONFIG = {}
+CONFIG.bridgeName = 'bridgeName'
+CONFIG.pinCode = 'pinCode'
+CONFIG.port = 'port'
+CONFIG.allowInsecureRequest = 'allowInsecureRequest'
+CONFIG.manufacturer = 'manufacturer'
+CONFIG.serialNo = 'serialNo'
+CONFIG.model = 'model'
+CONFIG.name = 'name'
+CONFIG.serviceName = 'Lightbulb'
+CONFIG.parentService = 'ParentService'
 
 module.exports = {
     RED: {
         settings: {
             available: function() {
-                return false;
-            }
+                return false
+            },
         },
         httpAdmin: {
-            get: function() {}
+            get: function() {},
         },
         auth: {
-            needsPermission: function() {}
+            needsPermission: function() {},
         },
         nodes: {
             registerType: function(name, init) {
-                init(CONFIG);
+                init(CONFIG)
             },
             createNode: function(node) {
-                node.id = "id";
-                node.log = function() {};
-                node.on = function() {};
-                node.status = function() {};
-                node.debug = function() {};
+                node.id = 'id'
+                node.log = function() {}
+                node.on = function() {}
+                node.status = function() {}
+                node.debug = function() {}
             },
             getNode: function() {
                 let node = {
-                    publish: function () {
-                    }
-                };
-                node.bridge = { addBridgedAccessories: function() {} };
-                node.bridge.bridgedAccessories = { 0: "0" };
+                    publish: function() {},
+                }
+                node.bridge = { addBridgedAccessories: function() {} }
+                node.bridge.bridgedAccessories = { 0: '0' }
                 node.service = {
-                    addLinkedService: function() {}
-                };
+                    addLinkedService: function() {},
+                }
                 node.accessory = {
                     addService: function(service) {
-                        return service;
-                    }
-                };
-                node.accessory.services = {};
-                node.bridgeNode = node;
-                return node;
-            }
-        }
-    }
-};
+                        return service
+                    },
+                }
+                node.accessory.services = {}
+                node.bridgeNode = node
+                return node
+            },
+        },
+    },
+}
