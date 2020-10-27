0696bfbe533c305b4b4a8a384c8538312d0c70d9
Shaquu <tadev3@gmail.com>
2020-10-27 02:04:13 +0100

    Remove preinstall script and move nodejs version verify to nrchkb boot

--- src/nrchkb.ts
+++ src/nrchkb.ts
@@ -1,10 +1,21 @@
 import { NodeAPI } from 'node-red'
 import * as path from 'path'
+import semver from 'semver'
 import { HAPStorage } from 'hap-nodejs'
 
 const debug = require('debug')('NRCHKB')
 
 module.exports = (RED: NodeAPI) => {
+    const requiredNodeVersion = "10.22.1";
+    const nodeVersion = process.version;
+
+    if (semver.gte(nodeVersion, requiredNodeVersion)) {
+        debug('Node.js version requirement met. Required ' + requiredNodeVersion + '. Installed ' + nodeVersion)
+    } else {
+        throw new RangeError('Node.js version requirement not met. Required ' + requiredNodeVersion + '. Installed ' + nodeVersion)
+    }
+
+
     const API = require('./lib/api')(RED)
 
     // Initialize our storage system
